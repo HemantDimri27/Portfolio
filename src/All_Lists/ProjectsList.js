9a7af04747cffbc976a39065bb26f2039d8072f1
HemantDimri27 <dimrihemant27@gmail.com>
2024-03-15 12:54:32 +0530

    portfoloio

---
+++ src/All_Lists/ProjectsList.js
@@ -0,0 +1,25 @@
+import img1 from '../assets/img1.png'
+import img2 from '../assets/weather.png'
+import img3 from '../assets/todoApp.png'
+import img4 from '../assets/img4.png'
+
+const ProjectsList = [
+    {
+        id: 2,
+        title: 'Current Weather WebApp',
+        link: 'https://weather-webapp-pro.vercel.app',
+        github: 'https://github.com/chiragjain307/Weather-WebApp',
+        image: img2
+    },
+    {
+        id: 3,
+        title: 'Todo WebApp',
+        link: 'https://todo-webapp-pro.vercel.app',
+        github: 'https://github.com/chiragjain307/Todo-WebApp',
+        image: img3
+    },
+        
+    
+]
+
+export default ProjectsList
